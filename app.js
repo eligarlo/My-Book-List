736d6fdb62224f3569a258174ca612991197b686
eligarlo <eligarlo.developer@gmail.com>
2019-09-24 21:17:01 +0300

    update the remove method catching the data attributes

--- app.js
+++ app.js
@@ -24,14 +24,20 @@ class UI {
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.isbn}</td>
-            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+            <td><a href="#" class="btn btn-danger btn-sm delete" data-isbn="${book.isbn}">X</a></td>
         `;
+
+        row.setAttribute("data-row", `${book.isbn}`);
 
         list.appendChild(row);
     }
 
     static deleteBook(el) {
-        el.parentElement.parentElement.remove();
+        if (el.classList.contains('delete')) {
+            document.querySelector(`tr[data-row="${el.dataset.isbn}"]`).remove();
+            return true;
+        }
+        return false;
     }
 
     static showAlert(message, className) {
@@ -161,17 +167,10 @@ document.querySelector('#book-form').addEventListener('submit', e => {
 });
 
 // Event: Remove a Book
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.delete').forEach(deleteButton => {
-        deleteButton.addEventListener('click', e => {
-            // Remove book from UI
-            UI.deleteBook(e.target);
+document.querySelector('#book-list').addEventListener('click' ,e => {
+    // Remove book from store
+    Store.removeBook(e.target.dataset.isbn);
 
-            // Remove book from store
-            Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-
-            // Show success message
-            UI.showAlert('Book Removed', 'success');
-        })
-    });
+    // Remove book from UI and Show success message
+    if (UI.deleteBook(e.target)) UI.showAlert('Book Removed', 'success');
 });
